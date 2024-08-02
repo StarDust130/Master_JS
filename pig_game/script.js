0c37458ab63dccbf4c9a148c7f3ce114d563a65d
ChandraShekhar <csyadav0513@gmail.com>
2024-08-02 10:46:28 +0530

    chore: Update player scores to start from 0

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+// Selecting elements
+const roll_dice = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
+
+// Player - 1
+const current_score_1 = document.querySelector('#current--0');
+
+
+// Player - 2
+const current_score_2 = document.querySelector('#current--1');
+
+
+
+
+
+
+// Roll Dice Logic
+
+const rollDice = function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  document.querySelector('.dice').classList.remove('hidden');
+  document.querySelector('.dice').src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    current_score_1.textContent = Number(current_score_1.textContent) + dice;
+  } else {
+    current_score_1.textContent = 0;
+  }
+};
+
+
+roll_dice.addEventListener('click', rollDice);
+
+
+// Hold Logic
+
