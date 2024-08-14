b11e96fcf6fdd8d7ead85a1fcd49d09e3e0228b8
ChandraShekhar <csyadav0513@gmail.com>
2024-08-14 17:20:40 +0530

    Refactor variable declarations and add football betting functionality

--- pizza/script.js
+++ pizza/script.js
@@ -1,110 +1,62 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// Coding Challenge #1 🤙
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-  orderDelivery: function ({ ...obj }) {
-    console.log(`${obj.starterIndex}`);
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`${ing1} is ${ing2} is better than ${ing3} `);
-  },
-  orderPizza: function (mainIng, ...otherIng) {
-    console.log(`${mainIng} is better than ${otherIng} `);
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
-
-//! Array
-// Receive 2 return value from a function.
-// const [starter, main] = restaurant.order(2, 0);
-
-// console.log(starter, main); // Garlic Bread Pizza
-
-// // Nested Destructuring Array
-// const nested = [2, 4, [5, 6, [7]]];
-
-// const [val1, , [val2, , [val3]]] = nested;
-
-// console.log(val1, val2, val3); // 2 5 7
-
-// // Default Array
-
-// const [p = 1, q = 3, r = 5] = [8, 9];
-// console.log(p, q, r); // 8 9 5
-
-// //! Object
-
-// const { name: naam, openingHours, categories, cat = 'bat' } = restaurant;
-
-// console.log(naam, openingHours, categories, cat);
-
-// // Mutating Variable
-
-// let a = 111;
-// let b = 222;
-// const obj = {
-//   a: 1,
-//   b: 2,
-//   c: 3,
-//   d: 4,
-// };
-
-// ({ a, b } = obj);
-
-// console.log(a, b);
-
-// // Nested Objects
-
-// const {
-//   fri: { open, close },
-// } = openingHours;
-// console.log(open, close);
-
-// const arr = 'babu';
-// const newArr = [...arr];
-
-// console.log(...arr);
-
-// const ingredients = ["rice" , "roti" , "cake"]
-
-// restaurant.orderPasta(...ingredients);
-
-// REST operator
-
-// const [a, b, ...others] = [1, 2, 3, 4, 5];
-
-// const [p, , r, ...o] = [...restaurant.mainMenu, ...restaurant.starterMenu];
-// console.log(p, r, o);
-
-
-restaurant.orderPizza("cake" , ["bake" , "jake"])
-
-
-//! Short 
