0b379a28bbae1c6974c719ebb29f63c08ccb9c35
ChandraShekhar <csyadav0513@gmail.com>
2024-08-20 01:29:42 +0530

    Refactor variable declarations and update check-in functionality

--- plane/script.js
+++ plane/script.js
@@ -2,123 +2,129 @@
 
 // Function in JS
 
-const flight = 'AB2123'; //! Not Change in Fn
-const stardust = {
-  name: 'Chandrashekhar', //! Change in Fn
-  passport: 12356235,
-};
-
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'AC1236';
-  passenger.name = 'Mr.' + passenger.name;
-
-  if (passenger.passport === 12356235) {
-    console.log('Have a Great Trip ✈️ 😊');
-  } else console.log('Wrong Passport ☠️');
-};
-
-// checkIn(flight, stardust);
-
-// console.log(flight); // Same beacuse Primative are only create copy
-// console.log(stardust); // Changed beacuse Objects store refernce to data
-
-// Example:
-// const flightNum = flight;
-// const passenger = joans;
-
-//! Summary: Passing a primative type as arguments is just crete copy that of that variable. But in Object they pass acutal refernce so they can change. 👍
-
-const newPAssport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000);
-  console.log(`New Passport: ${person.passport}`);
-};
-
-// newPAssport(stardust);
-// checkIn(flight, stardust);
-
-//! First Class and Higher-Order Functions.
-
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
-
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
-
-// Higher-Order-Functions 🙂
-const transformer = function (str, fn) {
-  console.log(`Transformer string: ${fn(str)}`);
-  console.log(`Transformer by: ${fn.name} Function`);
-};
-
-// transformer('JavaScript is Best', upperFirstWord);
-// transformer('JavaScript is Best', oneWord);
-
-//! Function that Return Functions
-
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
-//   };
+// const flight = 'AB2123'; //! Not Change in Fn
+// const stardust = {
+//   name: 'Chandrashekhar', //! Change in Fn
+//   passport: 12356235,
 // };
 
-// const greeterHey = greet('Hey');
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'AC1236';
+//   passenger.name = 'Mr.' + passenger.name;
 
-// greeterHey('Babu Bindas 😄');
+//   if (passenger.passport === 12356235) {
+//     console.log('Have a Great Trip ✈️ 😊');
+//   } else console.log('Wrong Passport ☠️');
+// };
 
-// //! Function that Return Functions (Arrow Function)
+// // checkIn(flight, stardust);
 
-// const greet = greeting => name => console.log(`${greeting} ${name}`);
+// // console.log(flight); // Same beacuse Primative are only create copy
+// // console.log(stardust); // Changed beacuse Objects store refernce to data
 
-// const greeterHey = greet('Hey');
+// // Example:
+// // const flightNum = flight;
+// // const passenger = joans;
 
-// greeterHey('Babu Bindas 😄');
+// //! Summary: Passing a primative type as arguments is just crete copy that of that variable. But in Object they pass acutal refernce so they can change. 👍
 
-//! The call and apply Methods
+// const newPAssport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000);
+//   console.log(`New Passport: ${person.passport}`);
+// };
 
-const airIndia = {
-  airline: 'AirIndia',
-  code: 'AI',
-  booking: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.code}${flightNum}`
-    );
-    this.booking.push({ flight: `${this.code}${flightNum}`, name });
-  },
-};
+// // newPAssport(stardust);
+// // checkIn(flight, stardust);
 
-airIndia.book(521581, 'Chandrashekhar');
+// //! First Class and Higher-Order Functions.
 
-const vistara = {
-  airline: 'Vistara',
-  code: 'VI',
-  booking: [],
-  //   book,
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const book = airIndia.book;
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-// //! .call method call book() Fn. and 1st arrgument is this keyword value
-// book.call(vistara, 19, 'Munna Thanos');
+// // Higher-Order-Functions 🙂
+// const transformer = function (str, fn) {
+//   console.log(`Transformer string: ${fn(str)}`);
+//   console.log(`Transformer by: ${fn.name} Function`);
+// };
 
-// //! Apply method same as .call but it not recieve arrgument but a array. ### NOT USE ANYMORE ###
+// // transformer('JavaScript is Best', upperFirstWord);
+// // transformer('JavaScript is Best', oneWord);
 
-// const flightData = [856, 'Jay shetty '];
-// book.apply(vistara, flightData);
+// //! Function that Return Functions
 
-// //! MODERN ES6+ Way to do this.
-// book.call(vistara, ...flightData);
+// // const greet = function (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting} ${name}`);
+// //   };
+// // };
 
-//! Blind Method -> Help to manual set this keyword to any function
+// // const greeterHey = greet('Hey');
 
-// const bookedAirIndia = book.bind(airIndia);
-// bookedAirIndia(5224, 'Goti');
+// // greeterHey('Babu Bindas 😄');
 
-const addTax = rate => value => value + value + rate;
+// // //! Function that Return Functions (Arrow Function)
 
-const addVat = addTax(0.23);
+// // const greet = greeting => name => console.log(`${greeting} ${name}`);
 
-console.log(addVat(10));
+// // const greeterHey = greet('Hey');
+
+// // greeterHey('Babu Bindas 😄');
+
+// //! The call and apply Methods
+
+// const airIndia = {
+//   airline: 'AirIndia',
+//   code: 'AI',
+//   booking: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.code}${flightNum}`
+//     );
+//     this.booking.push({ flight: `${this.code}${flightNum}`, name });
+//   },
+// };
+
+// airIndia.book(521581, 'Chandrashekhar');
+
+// const vistara = {
+//   airline: 'Vistara',
+//   code: 'VI',
+//   booking: [],
+//   //   book,
+// };
+
+// const book = airIndia.book;
+
+// // //! .call method call book() Fn. and 1st arrgument is this keyword value
+// // book.call(vistara, 19, 'Munna Thanos');
+
+// // //! Apply method same as .call but it not recieve arrgument but a array. ### NOT USE ANYMORE ###
+
+// // const flightData = [856, 'Jay shetty '];
+// // book.apply(vistara, flightData);
+
+// // //! MODERN ES6+ Way to do this.
+// // book.call(vistara, ...flightData);
+
+// //! Blind Method -> Help to manual set this keyword to any function
+
+// // const bookedAirIndia = book.bind(airIndia);
+// // bookedAirIndia(5224, 'Goti');
+
+// // const addTax = rate => value => value + value + rate;
+
+// // const addVat = addTax(0.23);
+
+// // console.log(addVat(10));
+
+//!  Immediately Invoked Function Expressions (IIFE)
+
+(() => {
+  console.log('Run Once Done! 🤑');
+})();
