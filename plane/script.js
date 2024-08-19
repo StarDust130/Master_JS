06cd68740c67a22e636eea8db0648a10c58d63c9
ChandraShekhar <csyadav0513@gmail.com>
2024-08-19 12:59:29 +0530

    Refactor variable declarations and add check-in functionality

--- plane/script.js
+++ plane/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // Function in JS
+
+const flight = 'AB2123';
+const stardust = {
+  name: 'Chandrashekhar',
+  passport: 12356235,
+};
+
+const checkIn = function (flightNum, passenger) {
+    
+};
+
+checkIn(flight, stardust);
