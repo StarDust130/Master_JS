60fea826d4880df26e2d556437e18643e598d4ef
ChandraShekhar <csyadav0513@gmail.com>
2024-08-20 11:06:07 +0530

    chore: Refactor variable declarations and update check-in functionality

--- array/script.js
+++ array/script.js
@@ -79,10 +79,30 @@
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
-// slice: -> Divide Array into Part(Create Copy of Array)
+//? slice: -> Divide Array into Part(Create Copy of Array)
 // console.log(arr.slice(1));
 
-// splice: -> delete part from orginal Array
-console.log(arr.splice(2));
-console.log(arr);
+//? splice: -> delete part from orginal Array
+// console.log(arr.splice(2));
+// console.log(arr);
 
+//? Reverse: -> reverse it. (Mutate)
+
+// console.log(arr.reverse());
+
+//? Concat: -> Add two Array.
+
+const arr2 = ['x', 'y', 'z'];
+// console.log(arr.concat(arr2));
+//            OR
+// console.log([...arr, ...arr2]);
+
+//? Join: -> Joins all array elements into a string.
+
+// console.log(arr.join("-"));
+
+//! The (at) Method.
+
+// console.log(arr[0]);  -> We can do same thing with (at):
+
+console.log(arr.at(-1));
