64a8c05e21bdada147b69097e64c0b6ddb396ca1
ChandraShekhar <csyadav0513@gmail.com>
2024-08-22 20:52:07 +0530

    refactor: Update array index.js file with dinner function for randomizing dinner options

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 "use strict";
 
-const arr = [1, 2, 3, 4 ,5];
+const arr = [1, 2, 3, 4, 5];
 
 const [a, b, c] = arr;
 
-console.log(a, b, c);
+// console.log(a, b, c);
 
-console.log(arr);
+// console.log(arr);
 
+const dinner = (...food) => {
+  const getRandomIndex = () => Math.floor(Math.random() * food.length);
+
+  let index1 = getRandomIndex();
+  let index2 = getRandomIndex() === index1 && getRandomIndex();
+
+  return `Today's dinner is ${food[index1]} and ${food[index2]}.`;
+};
+
+console.log(dinner("kheer", "roti", "puri"));
