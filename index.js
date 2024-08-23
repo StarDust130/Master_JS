03d973151df79d50f7a35eef4c69f12e1aede2b6
ChandraShekhar <csyadav0513@gmail.com>
2024-08-23 09:40:08 +0530

    refactor: Remove commented out code and console.log statements in index.js

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const [a, b, c] = arr;
 
 // console.log(arr);
 
-const dinner = (...food) => {
-  const getRandomIndex = () => Math.floor(Math.random() * food.length);
+// const dinner = (...food) => {
+//   const getRandomIndex = () => Math.floor(Math.random() * food.length);
 
-  let index1 = getRandomIndex();
-  let index2 = getRandomIndex() === index1 && getRandomIndex();
+//   let index1 = getRandomIndex();
+//   let index2 = getRandomIndex() === index1 && getRandomIndex();
 
-  return `Today's dinner is ${food[index1]} and ${food[index2]}.`;
-};
+//   return `Today's dinner is ${food[index1]} and ${food[index2]}.`;
+// };
 
-console.log(dinner("kheer", "roti", "puri"));
+// console.log(dinner("kheer", "roti", "puri"));
+
+//! Dates in JS
+
+// console.log(new Date(0));
+
+console.log(5_000_000);
