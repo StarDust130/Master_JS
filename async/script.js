f72d1002eba08b238e39172c71fdfac210805638
ChandraShekhar <csyadav0513@gmail.com>
2024-08-29 03:00:32 +0530

    Add JS

--- async/script.js
+++ async/script.js
@@ -4,3 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const req = new XMLHttpRequest();
+
+req.open('GET', 'https://restcountries.com/v3.1/name/india');
+
+req.send();
+
+req.addEventListener('load', () => {
+  const [data] = JSON.parse(req.responseText);
+  console.log(data);
+
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" alt="${
+    data.name.common
+  } Flag" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common} (${
+    data.altSpellings[1]
+  })</h3>
+        <h4 class="country__region">${data.region} - ${data.subregion}</h4>
+        <p class="country__row"><span>🏛️</span>Capital: ${data.capital[0]}</p>
+        <p class="country__row"><span>🗺️</span>Area: ${(
+          data.area / 1_000_000
+        ).toFixed(2)} million km²</p>
+        <p class="country__row"><span>👫</span>Population: ${(
+          +data.population / 1_000_000
+        ).toFixed(2)} million</p>
+        <p class="country__row"><span>🗣️</span>Languages: ${Object.values(
+          data.languages
+        ).join(', ')}</p>
+        <p class="country__row"><span>💰</span>Currency: ${
+          Object.values(data.currencies)[0].name
+        } (${Object.keys(data.currencies)[0]})</p>
+        <p class="country__row"><span>⌚</span>Timezones: ${data.timezones.join(
+          ', '
+        )}</p>
+        <p class="country__row"><span>🌍</span>Native Name: ${
+          data.name.nativeName[Object.keys(data.name.nativeName)[0]].common
+        }</p>
+        <p class="country__row"><span>🏞️</span>Borders: ${
+          data.borders ? data.borders.join(', ') : 'None'
+        }</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
