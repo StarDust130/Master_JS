3ed2e884338679fbc7e64c9b5d7c47845894c6ba
ChandraShekhar <csyadav0513@gmail.com>
2024-08-31 12:22:18 +0530

    refactor: Update API endpoint for fetching country data in script.js

--- async/script.js
+++ async/script.js
@@ -157,6 +157,13 @@
 //   console.error(error.message);
 // }
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
 
 //! Parllel Promise
 
@@ -174,13 +181,13 @@ const get3Countries = async function (c1, c2, c3) {
     // console.log([data1.capital, data2.capital, data3.capital]);
 
     const data = await Promise.all([
-      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
-      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
-      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
     ]);
     console.log(data.map(d => d[0].capital));
   } catch (err) {
     console.error(err);
   }
 };
-get3Countries('portugal', 'canada', 'tanzania');
+get3Countries('india', 'usa', 'japan');
