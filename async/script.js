283b844e16117e603f643794f343f2e6c76e1a2b
ChandraShekhar <csyadav0513@gmail.com>
2024-08-31 12:46:25 +0530

    refactor: Update API endpoint for fetching country data in script.js

--- async/script.js
+++ async/script.js
@@ -167,27 +167,51 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 
 //! Parllel Promise
 
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    // const [data1] = await getJSON(
-    //   `https://restcountries.eu/rest/v2/name/${c1}`
-    // );
-    // const [data2] = await getJSON(
-    //   `https://restcountries.eu/rest/v2/name/${c2}`
-    // );
-    // const [data3] = await getJSON(
-    //   `https://restcountries.eu/rest/v2/name/${c3}`
-    // );
-    // console.log([data1.capital, data2.capital, data3.capital]);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(
+//     //   `https://restcountries.eu/rest/v2/name/${c1}`
+//     // );
+//     // const [data2] = await getJSON(
+//     //   `https://restcountries.eu/rest/v2/name/${c2}`
+//     // );
+//     // const [data3] = await getJSON(
+//     //   `https://restcountries.eu/rest/v2/name/${c3}`
+//     // );
+//     // console.log([data1.capital, data2.capital, data3.capital]);
 
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-    console.log(data.map(d => d[0].capital));
-  } catch (err) {
-    console.error(err);
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+//     console.log(data.map(d => d[0].capital));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// get3Countries('india', 'usa', 'japan');
+
+// //! Promise.race: recieve a array of promsie and return a promsie
+// (async () => {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v3.1/name/india`),
+//     getJSON(`https://restcountries.com/v3.1/name/australia`),
+//     getJSON(`https://restcountries.com/v3.1/name/japan`),
+//   ]);
+
+//   console.log(res);
+// })();
+
+
+//! Create a Promise
+
+let myPromise = new Promise((resolve, reject) => {
+  let success = true; // Imagine we check something here
+
+  if (success) {
+    resolve('It worked!'); // This will happen if successful
+  } else {
+    reject('It failed!'); // This will happen if something goes wrong
   }
-};
-get3Countries('india', 'usa', 'japan');
+});
